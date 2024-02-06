a154e86186957462ffe1d50ca77ddb15a7e1a2b3
Fade-ing <fdlandriyanto@gmail.com>
2024-02-06 19:40:36 +0800

    app, dashboard, edit: Implement edit and update todo
    * Properly links to localhost:3000 so user can access it directly
    * Update on checkbox tick are not implemented yet, still WIP

--- app.js
+++ app.js
@@ -27,6 +27,33 @@ app.post('/add/todo', async (req, res) => {
   res.redirect('back')
 })
 
+app.get('/edit/todo/:id', async (req, res) => {
+  const todoId = parseInt(req.params.id);
+  const todo = await prisma.todo.findUnique({
+    where: {
+      id: todoId
+    }
+  });
+  res.render('edit', { todo });
+})
+
+app.post('/update/todo/:id', async (req, res) => {
+  const updateId = parseInt(req.params.id)
+  const { activity, description } = req.body
+  const status = req.body.status ? true : false
+  const updateData = await prisma.todo.update({
+    where: {
+      id: updateId
+    },
+    data: {
+      activity,
+      description,
+      status
+    }
+  })
+  res.redirect('/')
+})
+
 app.post('/delete/todo/:id', async (req, res) => {
   const deleteId = parseInt(req.params.id)
   await prisma.todo.delete({
@@ -38,5 +65,5 @@ app.post('/delete/todo/:id', async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`App listening on port localhost:${port}`)
+  console.log(`App listening on port http://localhost:${port}`)
 })
